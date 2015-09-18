05b618d11d79f541dc842050c8492fc7de037c45
camelCaseD <brandon@camelcased.com>
2015-09-18 15:33:42 -0700

    Complete adding form to shorten view. Also add route for unknown paths.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,11 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
+  $scope.loading = false;
   $scope.addLink = function () {
+    $scope.loading = true;
     Links.addLink($scope.link).then(function () {
-
+      $scope.loading = false;
     });
   };
 
