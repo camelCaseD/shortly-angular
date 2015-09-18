88a2592d07fbfbd82dc97d5f158b55d84c776863
camelCaseD <brandon@camelcased.com>
2015-09-18 16:25:01 -0700

    Add validation to shorten form

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,7 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
   $scope.loading = false;
+
   $scope.addLink = function () {
     $scope.loading = true;
     Links.addLink($scope.link).then(function () {
