ccc419b2625b8412f314bc7265d79a25ebc635fc
Brandon Borders <brandon@camelcased.com>
2015-09-19 11:32:02 -0700

    Move search bar into nav

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,7 +1,11 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links, $location, Auth) {
   $scope.data = {};
+  $scope.$location = $location;
+
+  $scope.logout = Auth.signout;
+
   $scope.getLinks = function () {
     Links.getLinks().then(function (response) {
       $scope.data.links = response.data;
